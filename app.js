8647c1bdac402e3ceed6d179ea1ab07761cdeba3
Juan Pablo Acosta <acosta.jp@icloud.com>
2022-04-05 20:53:50 -0400

    Functional backend

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 'use strict';
 const express = require('express');
+const https = require('https');
 
+const formatDate = function (date) {
+	return date.toISOString().split("T")[0];
+  };
+
+let to_date = new Date();
+let from_date = new Date();
+from_date.setDate(from_date.getDate() - 10);
 const api_key = 'LgMSmaBZAb4Ob7M36pdmsrt9PlYXzbx1dUHbsnck';
 
 const app = express();
@@ -13,5 +21,26 @@ app.listen(port);
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-	res.render('index', {key: api_key});
+	res.render('index')
 });
+
+app.get('/new-imgs', (req, res) => {
+	https.get(`https://api.nasa.gov/planetary/apod?api_key=${api_key}&start_date=${formatDate(from_date)}&end_date=${formatDate(to_date)}`, (resp) => {
+		to_date.setDate(to_date.getDate() - 10);
+		from_date.setDate(from_date.getDate() - 10);
+		let data = '';
+
+		resp.on('data', (chunk) => {
+			data += chunk;
+		});
+
+		resp.on('end', () => {
+			let imgs = JSON.parse(data);
+			res.send({ imgs })
+		});
+
+		}).on("error", (err) => {
+		console.log("Error: " + err.message);
+			
+		});
+})
